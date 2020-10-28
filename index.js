6f6ee50d727019699ba721e4db80603aec575e0f
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-10-28 20:10:14 +0100

    Get individual article
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, useNewUrlParser: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/articlesDB");
@@ -52,7 +52,22 @@ app
     });
   });
 
-//Listem for server
+/********************************************  All Routes to individual articles ****************************************/
+
+app.get("/articles/:articleTitle", function (req, res) {
+  Article.findOne({ title: req.params.articleTitle }, function (
+    err,
+    foundArticle
+  ) {
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No article with that title found!");
+    }
+  });
+});
+
+//Listen for server
 app.listen(3000, function () {
   console.log("Server started on port 3000!");
 });
