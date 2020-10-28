ecf967b7a2767a623eefb45c3ddbba0b050166d3
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-10-28 19:48:02 +0100

    DRY code using app.route multi route handler
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- index.js
+++ index.js
@@ -17,45 +17,42 @@ const articleSchema = mongoose.Schema({
 
 const Article = mongoose.model("Articles", articleSchema);
 
-//MAIN ROUTES
-
-//1. Fetch all articles
-app.get("/articles", function (req, res) {
-  Article.find({}, function (err, foundArticles) {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+/********************************************  All Routes to /articles ************************************************ */
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find({}, function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const article = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    article.save(function (err) {
+      if (!err) {
+        res.send("Successfully saved article!");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully deleted articles!");
+      } else {
+        res.send(err);
+      }
+    });
   });
-});
 
-//2. Post an article
-app.post("/articles", function (req, res) {
-  const article = new Article({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  article.save(function (err) {
-    if (!err) {
-      res.send("Successfully saved article!");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-//3.Delete articles
-app.delete("/articles", function (req, res) {
-  Article.deleteMany(function (err) {
-    if (!err) {
-      res.send("Successfully deleted articles!");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
+//Listem for server
 app.listen(3000, function () {
   console.log("Server started on port 3000!");
 });
